12799a95482e8e42e3eb9ed226c599f16c0b3ea7
Shaun Ramsey <shaun.ramsey@gmail.com>
2020-08-08 20:34:59 -0400

    AppDelegate updates along with map integration  for  iOS side of things

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,26 +1,15 @@
 import UIKit
 import Flutter
 import background_locator
-
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-}
-
-
-
+import google_maps_flutter
+  
 
 func registerPlugins(registry: FlutterPluginRegistry) -> () {
     if (!registry.hasPlugin("BackgroundLocatorPlugin")) {
         GeneratedPluginRegistrant.register(with: registry)
     }
 }
+ 
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -28,7 +17,14 @@ func registerPlugins(registry: FlutterPluginRegistry) -> () {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyB5A4tFZas3BSzgNiRmQ0Hk84U_khFuUAI")
     GeneratedPluginRegistrant.register(with: self)
     BackgroundLocatorPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+
+
+
+
