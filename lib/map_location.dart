fcaf8877c828057e80f22941d26d58679fc02e0e
shaunramsey <shaun.ramsey@gmail.com>
2020-09-02 16:20:44 -0400

    testing ios edits in map_location

--- lib/map_location.dart
+++ lib/map_location.dart
@@ -1,5 +1,7 @@
 
 import 'package:background_locator/background_locator.dart';
+import 'package:background_locator/settings/ios_settings.dart';
+import 'package:background_locator/settings/locator_settings.dart';
 //import 'package:background_locator/location_settings.dart';
 import 'package:location_permissions/location_permissions.dart' as location_permissions;
 import 'package:smartresponse4/location_callback_handler.dart';
